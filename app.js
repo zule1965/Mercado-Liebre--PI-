5684b235149277d9e59c4cd4395139b53b49f994
unknown <mzalvarez11@gmail.com>
2022-09-20 16:59:03 +0200

    cambios

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-let proy="ML";
-console.log(proy);
+const express= require('express');
+const { dirname } = require('path');
+const path= require ('path');
+const app= express();
+
+app.listen(3030,()=>
+    console.log('servidor corriendo')
+);
+app.get ('/'), (requ,res)=>{
+    res.sendFile(path.join(--dirname,'/views/index.html'))
+}
