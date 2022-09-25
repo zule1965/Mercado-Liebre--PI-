0d7e66d49f215f8663ce95d5602ebc319a641cc3
unknown <mzalvarez11@gmail.com>
2022-09-25 12:42:41 +0200

    imagenes

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express= require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, ('/public'))));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get ('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'/views/index.html'))
 })
 
-app.listen(3000,()=>
+app.listen(3001,()=>
     console.log('servidor corriendo en puerto 3001')
 );
 
